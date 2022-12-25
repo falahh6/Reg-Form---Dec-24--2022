73b0459b3abb8902e99534b8a4c2f3ece1a1eeda
falah <falah@duck.com>
2022-12-25 18:19:36 +0530

    loc add

--- script.js
+++
@@ -1,8 +0,0 @@
-let textToChangeElement = document.getElementById('text-to-change');
-
-function updateText() {
-    let currentText = textToChangeElement.innerHTML;
-    let nextText = 'Git/Github';
-    currentText.innerHTML = nextText;
-}
-setInterval(updateText, 3000);
